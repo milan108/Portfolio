69d3d283597a86c1abb03eb756a9655e80c30793
milan108 <milan.lastname@outlook.com>
2019-09-29 16:57:45 -0400

    added social imgs

--- code/portfolio/src/containers/Navigation/SocialNavBar.js
+++ code/portfolio/src/containers/Navigation/SocialNavBar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Nav.scss';
 import Nav from './Nav';
+import github_logo from '../../images/github_logo.png';
+import linkedin_logo from '../../images/linkedin_logo.png';
+import mobile from '../../images/mobile.png';
+import envelope from '../../images/envelope.png';
 
 const SocialNavBar = props => {
 	return (
 		<Nav>
 			<div className='Nav-Social'>
 				<ul>
-					<li>647-221-7265</li>
-					<li>milan.lastname@outlook.com</li>
-					<li>milan108</li>
-					<li>milan-m</li>
+					<li><span className='mobile'><img src={mobile}/></span>647-221-7265</li>
+					<li><span className='envelope'><img src={envelope}/></span>milan.lastname@outlook.com</li>
+					<li><img src={github_logo} />milan108</li>
+					<li><img src={linkedin_logo} />milan-m</li>
 				</ul>	
 			</div>
 		</Nav>
