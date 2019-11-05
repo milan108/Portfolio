1f2d4031b1f18bb0e4ca0dc6c0c9a92c87224648
milan108 <milan.lastname@outlook.com>
2019-11-05 18:07:35 -0500

    modded title with a little animation

--- code/portfolio/src/components/Title/Title.js
+++ code/portfolio/src/components/Title/Title.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Title.scss';
 import SocialNavBar from '../../containers/Navigation/SocialNavBar';
 
 const Title = props => {
+    const[word,setWord] = useState("");
+
+    const sleep =(ms)=> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    useEffect(() => {
+        const words = ["UX", "UI", "Experiences", "Dreams", "Ambitions", "Designs", "Customer Satisfaction", "Solutions", "Ecosystem"];
+
+        const cycleWords = async () => {
+            for (let i of words) {
+                setWord(i);
+                await sleep(300);
+            }
+            cycleWords();
+        }
+
+        cycleWords();
+
+    }, []);
+
     return (
         <div className="Title">
-            I would love to build your .
+            I'd love to help build your <br /> <span className="word">{word}.</span>
         </div>
     );
 }
