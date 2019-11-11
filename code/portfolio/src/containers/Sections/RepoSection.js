218dc4b00e566b0ea0b30a17cc38eab32c810e12
milan108 <milan.lastname@outlook.com>
2019-11-11 12:45:22 -0500

    fixed broken github link

--- code/portfolio/src/containers/Sections/RepoSection.js
+++ code/portfolio/src/containers/Sections/RepoSection.js
@@ -13,7 +13,7 @@ const RepoSection = props => {
                 <div className="row-lg RepoCards">
                     <RepoCard image = {img_algo} title = "Algorithms" description = "An collection of algorithms written in whatever language I please." url = "https://github.com/milan108/Algorithms" />
                     <RepoCard image = {img_payme} title = "PayMe" description = "Stripe payment integration sample." url = "https://github.com/milan108/pay-me" />
-                    <RepoCard image = {img_libgdx} title = "LibGDX Sampler" description = "A desktop sampler to complement the libGDX game engine." url = "https://github.com/milan108/Algorithms/libgdx-sampler" />
+                    <RepoCard image = {img_libgdx} title = "LibGDX Sampler" description = "A desktop sampler to complement the libGDX game engine." url = "https://github.com/milan108/libgdx-sampler" />
                     <RepoCard image = {img_bank} title = "Bank Nothing" description = "A mock banking app in early stages with a simple crypto implementaion." url = "https://github.com/milan108/bank-nothing" />
                 </div>
             <p>*  Although I work mostly as a frontend developer, most of the projects I've worked on are closed source and therefore not publicly available on my github.</p>
